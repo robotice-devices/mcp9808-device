869770e193868177a3fefc1f5746da0c07c94468
Ales Komarek <mail@newt.cz>
2015-01-03 17:01:15 +0100

    fix

--- mcp9808/MCP9808.py
+++ mcp9808/MCP9808.py
@@ -15,7 +15,7 @@ class MCP9808(object):
         val = hex(self._i2c.reverseByteOrder(self._i2c.readU16(0x06)))
         print val
         if val != 0x54:
-            logger.warning('Not right manufacturer (0x54): %s' val)
+            logger.warning('Not right manufacturer (0x54): %s' % val)
 #        assert val == 0x54, 'Not right manufacturer'
         val = hex(self._i2c.reverseByteOrder(self._i2c.readU16(0x07)))
         print val
